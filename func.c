c6f2b446023ddae958bbc40271107f752f434db4
vinicius <viniciusleeg@outlook.com>
2021-08-03 22:06:49 -0300

    att

--- func.c
+++ func.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int soma(int n1, int n2){
+int soma(int n1, int n2)
+{
   int res;
   res = n1 + n2;
   return res;
 }
 
-void ola (char *nome) {  // esta função não possiu retorno
+void ola(char *nome)
+{ // esta função não possiu retorno
   printf("Olá, %s\n", nome);
 }
 
-void tchau (char msg[]) {
+void tchau(char msg[])
+{
   fprintf(stderr, "ERRO: %s\n", msg);
   exit(1);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
   int num1, num2;
   char nome[10];
   int idade = 0;
@@ -26,13 +30,13 @@ int main(int argc, char *argv[]){
   printf("Digite o numero 2: ");
   scanf("%d", &num2);
 
-  printf("%d + %d = %d\n", num1, num2, soma(num1, num2)); 
+  printf("%d + %d = %d\n", num1, num2, soma(num1, num2));
   // caso em outro ponto do programa vc queira utilizar essa mesma estrutura, criar uma função
 
   printf("Digite seu nome: ");
   scanf("%s", nome);
   ola(nome);
-  
+
   printf("Digite sua idade: ");
   scanf("%d", &idade);
 
@@ -44,7 +48,4 @@ int main(int argc, char *argv[]){
   printf("Sua idade: %d\n", idade);
 
   return 0;
-
- }
-
-
+}
